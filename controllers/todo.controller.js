d7964736173dc5a1fdaeb530f18240f4b2f8c541
ReetaS <reeta.sonar@gmail.com>
2023-04-24 14:02:40 +0000

    api routes added

---
+++ controllers/todo.controller.js
@@ -0,0 +1,67 @@
+var TodoService = require('../services/todos.service')
+
+exports.getTodos = async function(req, res, next){
+    var page = req.query.page ? req.query.page : 1
+    var limit = req.query.limit ? req.query.limit : 10; 
+
+    console.log(page, limit)
+
+    try{
+        var todos = await TodoService.getTodos({}, page, limit)
+        return res.status(200).json({status: 200, data: todos, message: "Succesfully Todos Recieved"});
+    }catch(e){
+        return res.status(400).json({status: 400, message: e.message});
+    }
+}
+
+exports.createTodo = async function(req, res, next){
+    var todo = {
+        title: req.body.title,
+        isCompleted: req.body.isCompleted,
+        date: req.body.date
+    }
+
+    try{
+        var createdTodo = await TodoService.createTodo(todo)
+        return res.status(201).json({status: 201, data: createdTodo, message: "Succesfully Created ToDo"})
+    }catch(e){
+        return res.status(400).json({status: 400, message: "Todo Creation was Unsuccesfull"})
+    }
+}
+
+exports.updateTodo = async function(req, res, next){
+
+    if(!req.body._id){
+        return res.status(400).json({status: 400., message: "Id must be present"})
+    }
+
+    var id = req.body._id;
+    console.log("Update request body",req.body)
+
+    var todo = {
+        id,
+        title: req.body.title ? req.body.title : null,
+        isCompleted: req.body.isCompleted ? req.body.isCompleted : null,
+        date: req.body.date ? req.body.date : null
+    }
+
+    try{
+        var updatedTodo = await TodoService.updateTodo(todo)
+        return res.status(200).json({status: 200, data: updatedTodo, message: "Succesfully Updated Tod"})
+    }catch(e){
+        return res.status(400).json({status: 400., message: e.message})
+    }
+}
+
+exports.removeTodo = async function(req, res, next){
+
+    var id = req.params.id;
+
+    try{
+        var deleted = await TodoService.deleteTodo(id)
+        return res.status(204).json({status:204, message: "Succesfully Todo Deleted"})
+    }catch(e){
+        return res.status(400).json({status: 400, message: e.message})
+    }
+
+}
---
+++ services/todos.service.js
@@ -0,0 +1,75 @@
+var ToDo = require('../models/todo.model')
+
+_this = this
+
+
+exports.getTodos = async function(query, page, limit){
+    var options = {
+        page,
+        limit
+    }
+    try {
+        var todos = await ToDo.paginate(query, options)
+        return todos;
+    } catch (e) {
+        throw Error('Error while Paginating Todos')
+    }
+}
+
+exports.createTodo = async function(todo){
+
+    var newTodo = new ToDo({
+        title: todo.title,
+        isCompleted: todo.isCompleted,
+        date: new Date(),
+    })
+
+    try{
+        var savedTodo = await newTodo.save()
+        return savedTodo;
+    }catch(e){
+        throw Error("Error while Creating Todo")
+    }
+}
+
+exports.updateTodo = async function(todo){
+    var id = todo.id
+
+    try{
+        var oldTodo = await ToDo.findById(id);
+    }catch(e){
+        throw Error("Error occured while Finding the Todo")
+    }
+
+    if(!oldTodo){
+        return false;
+    }
+
+    console.log(oldTodo)
+
+    oldTodo.title = todo.title
+    oldTodo.isCompleted = todo.isCompleted
+
+
+    console.log(oldTodo)
+
+    try{
+        var savedTodo = await oldTodo.save()
+        return savedTodo;
+    }catch(e){
+        throw Error("And Error occured while updating the Todo");
+    }
+}
+
+exports.deleteTodo = async function(id){
+    
+    try{
+        var deleted = await ToDo.remove({_id: id})
+        if(deleted.result.n === 0){
+            throw Error("Todo Could not be deleted")
+        }
+        return deleted
+    }catch(e){
+        throw Error("Error Occured while Deleting the Todo")
+    }
+}
